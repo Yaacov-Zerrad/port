272fe8e6127cd3e80d0f6f22b06c56d1e11ddd60
Yaacov Zerrad <yaazerrad@gmail.com>
2022-08-07 21:52:27 +0300

    test info

--- portfolio/core/settings.py
+++ portfolio/core/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [templates],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
