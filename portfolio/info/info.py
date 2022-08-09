fa8375fac5bcd1df7e4c5c79d3854abbddc18f37
Yaacov-Zerrad <101695734+Yaacov-Zerrad@users.noreply.github.com>
2022-08-09 20:50:34 +0300

    Update info.py

--- portfolio/info/info.py
+++ portfolio/info/info.py
@@ -3,5 +3,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 # your email
 EMAIL_HOST_USER = 'z0583214808@gmail.com'
 # your application password (search 'application password gmail' in your search term)
-EMAIL_HOST_PASSWORD = 'hgzxlnfppifxrtjx'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
