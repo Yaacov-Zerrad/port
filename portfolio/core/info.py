9a89ba4e1537a1202f3b807bf80277ff3c761b03
Yaacov-Zerrad <101695734+Yaacov-Zerrad@users.noreply.github.com>
2022-08-16 12:36:57 +0300

    delete info

--- portfolio/core/info.py
+++
@@ -1,7 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-# your email
-EMAIL_HOST_USER = 'z0583214808@gmail.com'
-# your application password (search 'application password gmail' in your search term)
-EMAIL_HOST_PASSWORD = 'hgzxlnfppifxrtjx'
-EMAIL_PORT = 587
